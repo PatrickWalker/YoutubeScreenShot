eb249fddbb82a583fcabff0c43771aa8e41af3aa
Patrick Walker <bpatrick.walker@gmail.com>
2019-03-18 23:10:47 +0000

    added cli

--- index.js
+++ index.js
@@ -1,45 +1,79 @@
 const puppeteer = require('puppeteer');
+const inquirer = require("inquirer");
+const chalk = require("chalk");
 
-(async () => {
 
-const browser = await puppeteer.launch();
+const scrape = async (url, screendir) => {
 
-const page = await browser.newPage();
-//take this from command line
-const url = 'https://www.youtube.com/watch?v=N24fVEJyQKM';
-const interval = 1000;
-await page.goto(url);
-const video = await page.$('.html5-video-player')
-await page.content();
+    const browser = await puppeteer.launch();
 
-let count = 0;
-const currentEl =  await page.$('span.ytp-time-current');
-const durEl = await page.$('span.ytp-time-duration');
-console.log('Processing : ' + url);
+    const page = await browser.newPage();
+    //take this from command line
+    //const url = 'https://www.youtube.com/watch?v=N24fVEJyQKM';
+    const interval = 1000;
+    await page.goto(url);
+    const video = await page.$('.html5-video-player')
+    await page.content();
 
-setTimeout(async () => {
-    await page.click('.html5-video-player')
-    await page.keyboard.press('C');
-    await page.keyboard.press('Space');
+    let count = 0;
+    const currentEl =  await page.$('span.ytp-time-current');
+    const durEl = await page.$('span.ytp-time-duration');
+    console.log('Processing : ' + url);
 
-}
-,interval)
+    setTimeout(async () => {
+        await page.click('.html5-video-player')
+        await page.keyboard.press('C');
+        await page.keyboard.press('Space');
 
-let timerId = setInterval(async () => {
-    const curTime =  await page.evaluate(el => el.innerText, currentEl);
-    const durTime =  await page.evaluate(el => el.innerText, durEl);
-    if ( curTime >= durTime){
-        console.log('Finished processing video');
-        browser.close();
-        clearInterval(timerId);
-        return;
     }
+    ,interval)
 
-    video.screenshot({path: './screenshots/screenshot' + count +  '.png'});
-    count++;
-}
-, interval);
+    let timerId = setInterval(async () => {
+        const curTime =  await page.evaluate(el => el.innerText, currentEl);
+        const durTime =  await page.evaluate(el => el.innerText, durEl);
+        if ( curTime >= durTime){
+            console.log('Finished processing video');
+            browser.close();
+            clearInterval(timerId);
+            return;
+        }
+
+        video.screenshot({path: screendir + '/screenshot' + count +  '.png'});
+        count++;
+    }
+    , interval);
 
 
 
-})();
+};
+  
+
+  const askQuestions = () => {
+    const questions = [
+      {
+        name: "URL",
+        type: "input",
+        message: "What is the url of the youtube video to screenshot?"
+      },
+      {
+        type: "input",
+        name: "DIR",
+        message: "What is the screenshot directory?",
+        default: "./screenshots"
+    }
+    ];
+    return inquirer.prompt(questions);
+  };
+
+  const run = async () => {
+
+    // ask questions
+    const answers = await askQuestions();
+    const { URL, DIR } = answers;
+    // create the file
+    scrape(URL,DIR);
+    // show success message
+  };
+
+run();
+
